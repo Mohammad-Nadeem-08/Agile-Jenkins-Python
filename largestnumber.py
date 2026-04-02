1c324f8fa6a58965b1cb1707fc84f3bf15ab2cab
Mohammad-Nadeem-08 <mohammadnadeem0807@gmail.com>
2026-04-02 09:41:49 +0530

    some changes in largest file

--- largestnumber.py
+++ largestnumber.py
@@ -1,8 +1,9 @@
 a=10
 b=20
 c=5
+print("hello")
 if(a>b and a>c):
-    print(f"{a} is Greater then {b} and {c}")
+    print(f"{a} is Greater then {c} and {c}")
 elif(b>a and b>c):
     print(f"{b} is Greater then {a} and {c}")
 else:
