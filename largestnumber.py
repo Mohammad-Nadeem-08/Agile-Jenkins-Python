99abf186c8784aff1286102005e584c935dcf06c
Mohammad-Nadeem-08 <mohammadnadeem0807@gmail.com>
2026-04-02 08:53:45 +0530

    largest code is added

---
+++ largestnumber.py
@@ -0,0 +1,13 @@
+a=10
+b=20
+c=5
+if(a>b and a>c):
+    print(f"{a} is Greater then {b} and {c}")
+elif(b>a and b>c):
+    print(f"{b} is Greater then {a} and {c}")
+else:
+    print(f"{c} is Greater then {a} and {b}")
+
+
+
+
